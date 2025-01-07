e2fc09e9ed53cb0fbb126fce563bfa0eae43dc5a
anko9801 <daiusa3@icloud.com>
2025-01-07 22:10:01 +0900

    🎸 feat: 作品詳細ページでリクエストを利活用する

--- workspaces/app/src/features/episode/components/EpisodeListItem.tsx
+++ workspaces/app/src/features/episode/components/EpisodeListItem.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import type { GetEpisodeListResponse } from '@wsh-2024/schema/src/api/episodes/GetEpisodeListResponse';
+
 import { Box } from '../../../foundation/components/Box';
 import { Flex } from '../../../foundation/components/Flex';
 import { Image } from '../../../foundation/components/Image';
@@ -29,12 +31,10 @@ const _ImgWrapper = styled.div`
 
 type Props = {
   bookId: string;
-  episodeId: string;
+  episode: GetEpisodeListResponse[number];
 };
 
-export const EpisodeListItem: React.FC<Props> = ({ bookId, episodeId }) => {
-  const { data: episode } = useEpisode({ params: { episodeId } });
-
+export const EpisodeListItem: React.FC<Props> = ({ bookId, episode }) => {
   const imageUrl = useImage({ height: 96, imageId: episode.image.id, width: 96 });
 
   return (
